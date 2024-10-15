cb32a07f8d7f5770d4ee9cf01a7fb734c2f97499
ParkhMinjun <hm980411@gmail.com>
2024-10-15 16:53:03 +0900

    practice

---
+++ 연습문제(10.15)/소스.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h> 
+
+int main(void)
+{
+	char o;
+	int a, b, res;
+
+	printf("사칙연산 입력(정수):");
+	scanf("%d %c %d", &a, &o, &b);
+
+	switch (o)
+	{
+		{case '+': res = a + b;     // + 기호이면 덧셈 후 빠져나오기
+				 break; }
+
+		{case '-': res = a - b;     // - 기호이면 뺄셈 후 빠져나오기
+				 break; }
+
+		{case '*': res = a * b;     // * 기호이면 곱셈 후 빠져나오기
+				 break; }
+
+		{case '/': res = a / b;    // / 기호이면 나눗셈 후 빠져나오기
+				 break; }
+		{default:; }
+			 
+	}
+
+	printf("%d %c %d = %d\n", a, o, b, res);
+
+	return 0;
+}
